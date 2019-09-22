a0dbe414f67027a495a923595a647a3d130e4a49
Carlos Henrique de Oliveira <choliveira84@gmail.com>
2019-09-22 10:30:00 -0300

    Testes

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import ReposContainer from './components/github/ReposContainer'
+import ReposList from './components/github/ReposList'
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
+  const divRepo = document.createElement('div');
   ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders Repos without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<ReposContainer />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders ReposList without crashing', () => {
+  const array = [{ 'id': 1, 'name': 'repo1' }];
+  const div = document.createElement('div');
+  ReactDOM.render(<ReposList repos={array} />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
